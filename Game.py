fe3356c85317fca1e7a5c4f2fd5b522a7c444bc3
shray0 <115892675+shray0@users.noreply.github.com>
2023-01-14 16:02:55 -0500

    created pipe class

--- Game.py
+++ Game.py
@@ -2,6 +2,7 @@ import pygame
 import neat
 import os
 import time
+import random
 
 #window frame constants
 WIN_WIDTH = 560
@@ -90,6 +91,43 @@ class Bird:
 
     def get_mask(self):
         return pygame.mask.from_surface(self.img)
+
+class Pipe:
+    GAP = 200
+    VELOCITY = 5
+    def __init__(self,x):
+        self.x = x
+        self.height = 0
+        self.gap = 100
+        self.top = 0
+        self.bottom = 0
+        self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
+        self.PIPE_BOTTOM = PIPE_IMG
+        self.past = False
+        self.top = self.height - self.PIPE_TOP.get_height()
+        self.bottom = self.height + self.GAP
+
+    def set_height(self):
+        self.height = random.randrange(50,450)
+    
+    def move(self):
+        self.x -= self.VELOCITY # setting the movement of pipe equal to negative velocity
+
+    def draw(self,window):
+        window.blit(self.PIPE_TOP,(self.x, self.top))
+        window.blit(self.PIPE_BOTTOM,(self.x, self.bottom))
+    
+    def collide(self, bird):
+        bird_mask = bird.get_mask()
+        top_masks = pygame.mask.from_surface(self.PIPE_TOP)
+        bottom_masks = pygame.mask.from_surface(self.PIPE_BOTTOM)
+        top_offset = (self.x-bird.x,self.top - round(bird.y))
+        bottom_offset = (self.x-bird.x,self.bottom - round(bird.y))
+        b_point = bird_mask.overlap(bottom_masks, bottom_offset)
+        t_point = bird_mask.overlap(top_masks, top_offset)
+        if t_point or b_point:
+            return True
+        return False
 
 
 def draw_window(win,bird):           #paremeters are the inputs the function requires
